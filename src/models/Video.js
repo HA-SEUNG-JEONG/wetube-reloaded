54ca8922cfdffa6742f117269c528e77d6e170a0
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-10-26 21:59:50 +0900

    Creating a Video part One

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
   title: String,
   description: String,
-  createdAt: Date,
+  createdDate: Date,
   hashtags: [{ type: String }],
   meta: {
     views: Number,
