dc782c7962d48d7994f7c92e3bc68ad4a85fd394
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-10-26 21:25:49 +0900

    Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -20,7 +20,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 1,
+    views: 59,
     id: 3,
   },
 ];
@@ -31,9 +31,11 @@ export const trending = (req, res) => {
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`,video});
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {};
