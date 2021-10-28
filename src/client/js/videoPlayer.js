7f9579db0ae0edc4b0743e346afd29739f8f7936
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-10-28 10:34:23 +0900

    Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,12 @@ const mutebutton = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+video.volume = 0.5;
+
+let volumeValue = 0.5; //전역 변수
+video.volume = volumeValue;
+// console.log("video.volume", video.volume);
+
 const handlePlayClick = (event) => {
   //if the video is playing, pause it
   if (video.paused) {
@@ -25,8 +31,23 @@ const handleMute = (event) => {
     video.muted = true;
   }
   mutebutton.innerText = video.muted ? "UnMute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    mutebutton.innerText = "Mute";
+  }
+  volumeValue = value;
+  //   console.log("volumeValue", volumeValue);
+  video.volume = value;
+  //   console.log("video.volume", video.volume);
 };
 
 playbutton.addEventListener("click", handlePlayClick);
 mutebutton.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
