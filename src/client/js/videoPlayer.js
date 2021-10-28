96d276329279ba1d59ba2b3cee9b9ae723dd3826
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-10-28 09:51:32 +0900

    Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,26 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playbutton = document.getElementById("play");
+const mutebutton = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (event) => {
+  //if the video is playing, pause it
+  if (video.paused) {
+    video.play();
+  }
+  //else play the video
+  else {
+    video.pause();
+  }
+};
+
+const handlePause = () => (playbutton.innerText = "Play");
+const handlePlay = () => (playbutton.innerText = "Pause");
+
+const handleMute = (event) => {};
+
+playbutton.addEventListener("click", handlePlayClick);
+mutebutton.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
