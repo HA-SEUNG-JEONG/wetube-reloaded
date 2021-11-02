c4b26a39d94c5f953ff4cbfdd7c4fa066d2eee93
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-11-02 16:48:23 +0900

    Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,12 +2,43 @@ import regeneratorRuntime from "regenerator-runtime";
 const startbutton = document.getElementById("startbutton");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startbutton.innerText = "Start Recording";
+  startbutton.removeEventListener("click", handleStop);
+  startbutton.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startbutton.innerText = "Stop Recording";
+  startbutton.removeEventListener("click", handleStart);
+  startbutton.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("recording done");
+    console.log(event);
+    console.log(event.data);
+  };
+
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 100, height: 100 },
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
+
 startbutton.addEventListener("click", handleStart);
