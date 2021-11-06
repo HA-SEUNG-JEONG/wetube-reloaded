e4918b9b10cb3ae19e337049d986ccce40d9d4ea
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-11-06 09:38:03 +0900

    Download Transcoded Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,4 +1,5 @@
 import regeneratorRuntime from "regenerator-runtime";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startbutton = document.getElementById("startbutton");
 const video = document.getElementById("preview");
 
@@ -6,11 +7,27 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({
+    log: true,
+    corePath: "/assets/ffmpeg-core.js",
+  });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mkv" });
+
+  const mp4URL = URL.createObjectURL(mp4Blob);
+
   const a = document.createElement("a");
   a.href = videoFile;
   //download 태그: 사용자로 하여금 URL을 통해 어디로 보내주는 게 아니라 URL을 저장하게 해준다.
-  a.download = "My Recording.webm";
+  a.download = "My Recording.mkv";
   document.body.appendChild(a);
   a.click();
 };
@@ -41,6 +58,7 @@ const handleStart = () => {
 };
 
 const init = async () => {
+  //mediaDevices는 promise 반환
   stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 100, height: 100 },
