03c9ada5d9b4697b5918da270b78c0e019295db3
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-11-07 10:26:03 +0900

    Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const fakeComments = document.createElement("li");
+  fakeComments.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  fakeComments.appendChild(icon); //i 태그 안에 있는 icon
+  fakeComments.appendChild(span); //span 안에 들어있는 댓글 내용
+  // console.log("fakecomments:", fakeComments); //li 태그 안에 icon과 span 내 내용이 들어감
+  videoComments.prepend(fakeComments); //prepend를 쓰면 최신 댓글이 맨 위로 올라옴
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -9,7 +23,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,7 +31,9 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
-  window.location.reload(); //댓글을 달면 실시간"처럼" 달리게 함
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {
