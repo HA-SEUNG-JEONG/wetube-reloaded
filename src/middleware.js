4c4d0d85090ec0f6d0b2c0c21d6d29852d9cf436
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-11-22 16:45:04 +0900

    Content Type 추가

--- src/middleware.js
+++ src/middleware.js
@@ -13,6 +13,7 @@ const multerUploader = multerS3({
   s3: s3,
   bucket: "wetubeeeee",
   acl: "public-read",
+  contentType: multerS3.AUTO_CONTENT_TYPE,
 });
 
 export const localsMiddleware = (req, res, next) => {
